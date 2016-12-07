2049dda34485d9230d0c6a4846dfc78ad1885c33
bdauer <dauer.benjamin@gmail.com>
2016-12-07 11:21:14 -0500

    Add Node interface

---
+++ src/graph/Node.java
@@ -0,0 +1,22 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ben Dauer
+ * 
+ * A Node interface to be implemented
+ * with the Graph interface.
+ *
+ */
+public interface Node {
+	
+	public void addNeighbor();
+	
+	public List<Integer> getNeighbors();
+	
+	public void getName();
+
+
+}
