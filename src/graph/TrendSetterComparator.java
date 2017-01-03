0df8edfcfcd5fa60ad84da9331a71502d20ee588
bdauer <dauer.benjamin@gmail.com>
2017-01-03 09:06:15 -0500

    add comparator for priorityqueue used for finding trend setters

---
+++ src/graph/TrendSetterComparator.java
@@ -0,0 +1,19 @@
+package graph;
+
+import java.util.Comparator;
+
+/**
+ * 
+ * @author Ben Dauer
+ * 
+ * Compares the number of neighbors
+ * between two nodes. 
+ *
+ */
+public class TrendSetterComparator implements Comparator<CapNode> {
+	
+	@Override
+	public int compare(CapNode node1, CapNode node2) {
+		return Double.compare(node2.getNumNeighbors(), node1.getNumNeighbors());
+	}
+}
